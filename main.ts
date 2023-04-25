d675a155b6470b279e28af4c9fb7ffe8c0148631
tonyoryo <131649057+tonyoryo@users.noreply.github.com>
2023-04-25 12:18:50 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    radio.sendString("")
+    radio.sendString("piedra")
+    basic.showIcon(IconNames.SmallSquare)
+    Mio = 2
 })
 input.onGesture(Gesture.Shake, function () {
-    tool = randint(0, 2)
-    if (tool == 0) {
-        basic.showIcon(IconNames.SmallSquare)
-    } else if (tool == 1) {
-        basic.showIcon(IconNames.Square)
-    } else {
-        basic.showIcon(IconNames.Scissors)
+    basic.clearScreen()
+    radio.sendString("papel")
+    basic.showIcon(IconNames.Square)
+    Mio = 1
+})
+radio.onReceivedString(function (receivedString) {
+    if (Mio == 1) {
+    	
+    }
+    if (Mio == 2) {
+    	
+    }
+    if (Mio == 3) {
+    	
     }
 })
-let tool = 0
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendString("tijera")
+    basic.showIcon(IconNames.Scissors)
+    Mio = 3
+})
+let Mio = 0
 radio.setGroup(6)
